f868afceb27d51608ef239ed60f74d606d9e02d5
Ronald Main <rbmain84@yahoo.com>
2021-08-01 20:53:04 -0500

    about

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import Nav from './components/Nav';
+import About from './components/About';
+
+function App() {
+
+  return (
+    <div>
+      <Nav />
+      <main>
+          <About />        
+      </main>
+    </div>
+  );
+}
+
+export default App;
+
